cc3f6add4495a9dedbceba14e697c3cfdc153029
roman <roman.slovak@gmail.com>
2023-11-01 14:52:28 +0100

    add state

--- app.py
+++ app.py
@@ -7,38 +7,30 @@ app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-gpio_list = [5,6,13,16,19,20,21,26]
 gpio_dict = {5:False, 6:False, 13:False, 16:False, 19:False, 20:False, 21:False, 26:False}
 
-for i in gpio_list:
+for i in gpio_dict:
     GPIO.setup(i, GPIO.OUT)
-    GPIO.output(i, GPIO.HIGH)
+    if gpio_dict[i] == False:
+        GPIO.output(i, GPIO.HIGH)
+    else:
+        GPIO.output(i, GPIO.LOW)
+
 
 @app.route('/')
 def home():
-    return render_template('index.html', gpio_list=gpio_list)
-
-@app.route('/gpio_control', methods=['GET', 'POST'])
-def gpio_control():
-    if request.method == 'POST':
-        # Get the form data
-        pin = request.form['pin']
-        state = request.form['state']
-        # Do something with the form data, like control the GPIO pins
-        # ...
-        # Render the template with the updated GPIO list
-        return render_template('index.html', gpio_list=gpio_list)
-    else:
-        # Render the template with the initial GPIO list
-        return render_template('index.html', gpio_list=gpio_list)
+    print(gpio_dict)
+    return render_template('index.html', gpio_list=gpio_dict)
 
 @app.route("/gpio/<int:pin>", methods=["POST"])
 def toggle_pin(pin):
     state = request.json["state"]
     if state == "On":
         GPIO.output(pin, GPIO.HIGH)
+        gpio_dict[pin] = True  
     else:
         GPIO.output(pin, GPIO.LOW)
+        gpio_dict[pin] = False 
     return jsonify({"success": True})
 
 @app.route('/on/<int:gpio>')
